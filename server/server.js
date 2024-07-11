7d19617f97bbf9c1ae60e1d2c5898be35be41837
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-07-11 16:22:00 +0800

    “feat: git viewer demo page/ yml of deploy to EC2”

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
+const appDirectory = process.cwd(); // 當前工作資料夾
+
+// 設定靜態文件路徑
+app.use(express.static(path.join(appDirectory, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(appDirectory, 'public', 'index.html'));
 });
 
 if (process.env.NODE_ENV !== 'test') {
